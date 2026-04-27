0a5b2a0d40fc26f18de6e7acf0d117fc8725464f
unknown <avnichandan2465@gmail.com>
2026-04-27 23:47:25 +0530

    Added Brute Force to Day3

--- Arrays/Buy-sell.java
+++ Arrays/Buy-sell.java
@@ -1,3 +1,8 @@
+//Problem: Best Time to Buy and Sell Stock
+//Approach: One Pass
+//Time Complexity: O(n)
+//Space Complexity: O(1)
+
 class Solution {
     public int maxProfit(int[] prices) {
         int maxProfit = 0;
@@ -10,6 +15,26 @@ class Solution {
             bestBuy = Math.min(bestBuy, prices[i]);
           
             
+        }
+        return maxProfit;
+    }
+}
+
+//Problem: Best Time to Buy and Sell Stock
+//Approach: Brute Force
+//Time Complexity: O(n^2)
+//Space Complexity: O(1)
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        int maxProfit = 0;
+
+        for(int i=0; i<prices.length; i++){
+            for(int j=i+1; j<prices.length; j++){
+                if(prices[j] > prices[i]){
+                    maxProfit = Math.max(maxProfit, prices[j] - prices[i]);
+                }
+            }
         }
         return maxProfit;
     }
