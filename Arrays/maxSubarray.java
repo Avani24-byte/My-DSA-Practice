fab0ab5ce9396ee61cb0e3e6a810d7a04beee902
unknown <avnichandan2465@gmail.com>
2026-04-28 23:48:17 +0530

    Day 4- Maximum Subarray

---
+++ Arrays/maxSubarray.java
@@ -0,0 +1,25 @@
+//Problem: Maximum Subarray
+//Approach: Kadane's Algorithm
+//Time Complexity: O(n)
+// Space Complexity: O(n)
+
+class Solution {
+    public int maxSubArray(int[] nums) {
+        
+        int sum = nums[0];
+        int maxSum = nums[0];
+
+        for(int i= 1;i < nums.length;i++){
+            if(sum >= 0){
+                sum = sum + nums[i];
+            }
+            else{
+                sum = nums[i];
+            }
+            if(sum > maxSum){
+                maxSum = sum;
+            }
+        }
+        return maxSum;
+    }
+}
