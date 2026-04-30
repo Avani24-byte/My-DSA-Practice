b1158d40063d2efeb34351685310f381f6842fca
unknown <avnichandan2465@gmail.com>
2026-05-01 00:58:22 +0530

    Day5 - Move Zeros

---
+++ Arrays/Move_zeros.java
@@ -0,0 +1,29 @@
+//Problem: Move Zeroes
+//Approach: Two Pointers
+//Time Complexity: O(n)
+//Space Complexity: O(1)
+
+class Solution {
+    public void moveZeroes(int[] nums) {
+    
+    int size = nums.length;
+    if(size == 0 || size == 1){
+        return;
+    }
+
+    int nz=0, z=0;
+
+    while(nz < size){
+        if(nums[nz] != 0){
+            int temp = nums[nz];
+            nums[nz] = nums[z];
+            nums[z] = temp;
+            nz++;
+            z++;
+        }
+        else{
+            nz++;
+        }
+    }
+    }
+}
