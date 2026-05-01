cdd74b4874b2ca92326bd62c9f80846faa4297d1
unknown <avnichandan2465@gmail.com>
2026-05-01 23:56:55 +0530

    Day6 - Product of Array except self

---
+++ Arrays/Product_array_except_self.java
@@ -0,0 +1,32 @@
+//Problem: Product of Array Except Self
+//Approach: Prefix and Suffix Product
+//Time Complexity: O(n)
+//Space Complexity: O(n)
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int n =nums.length;
+
+        int[] ans = new int[n];
+        int[] prefix = new int[n];
+        int[] suffix = new int[n];
+
+        Arrays.fill(prefix, 1);
+        Arrays.fill(suffix, 1);
+
+        //prefix
+        for(int i=1;i<n;i++){
+            prefix[i]= prefix[i-1] * nums[i-1];
+        }
+
+        //suffix
+        for(int i=n-2;i>=0;i--){
+            suffix[i]= suffix[i+1] * nums[i+1];
+        }
+
+         for(int i=0;i<n;i++){
+            ans[i]= prefix[i] * suffix[i];
+        }
+        return ans;
+        
+    }
+}
