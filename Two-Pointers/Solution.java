2e9f03b86328e7e6dee8d893b4b8119f030edd04
unknown <avnichandan2465@gmail.com>
2026-05-04 00:52:20 +0530

    Day 7- Container with most water(BF)

--- Two-Pointers/Solution.java
+++ Two-Pointers/Solution.java
@@ -28,3 +28,28 @@ public class Solution {
         return maxWater;
     }
 }
+
+//Problem: Container With Most Water
+//Approach: Brute Force
+//Difficulty: Medium
+//Time Complexity: O(n^2)
+//Space Complexity: O(1)
+
+class Solution {
+    public int maxArea(int[] height) {
+        int maxWater = 0;
+        int n = height.length;
+
+        for(int i = 0; i<n;i++){
+            for(int j= i+1; j<n;j++){
+                int w= j-i;
+                int ht= Math.min(height[j],height[i]);
+                int currWater= w * ht;
+
+                maxWater = Math.max(currWater, maxWater);
+
+            }
+        }
+        return maxWater;
+    }
+}
